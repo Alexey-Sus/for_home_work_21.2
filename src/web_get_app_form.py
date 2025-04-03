d93e2873a6bd993bb5af6e28f11a2ef77dfc21a5
Al_Pl <asuspitsyn@mail.ru>
2025-04-03 13:59:34 +0300

    Added files to the project, including the main working file, the needed html files and the css style files.

--- src/web_get_app_form.py
+++ src/web_get_app_form.py
@@ -38,52 +38,6 @@ class MyServer(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(f"Внутренняя ошибка сервера: {e}".encode("utf-8"))
 
-
-
-
-        # if self.path == "/redirect":
-        #     self.send_response(302)
-        #     self.send_header("Location", "/html_content")
-        #     self.end_headers()
-        # elif self.path == "/html_content":
-        #
-        #     remote_file_url = "https://raw.githubusercontent.com/Alexey-Sus/for_DataBase_project/develop/src/file.html"
-        #
-        #     try:
-        #         response = requests.get(remote_file_url)
-        #         response.raise_for_status()
-        #
-        #         with tempfile.NamedTemporaryFile(mode='w+t', delete=False,
-        #                                          encoding='utf-8') as tmp_file:
-        #             tmp_file.write(response.text)
-        #             tmp_file_path = tmp_file.name
-        #
-        #             try:
-        #                 with open(tmp_file_path, 'r', encoding='utf-8') as f:
-        #
-        #                     html_content = f.read()
-        #                     self.send_response(200)
-        #                     self.send_header("Content-type", "text/html")
-        #                     self.end_headers()
-        #                     self.wfile.write(html_content.encode("utf-8"))
-        #             finally:
-        #                 os.remove(tmp_file_path)
-        #
-        #     except requests.exceptions.RequestException as e:
-        #         self.send_response(500)
-        #         self.end_headers()
-        #         self.wfile.write((f"Ошибка при получении удаленного файла: {e}".encode("utf-8")))
-        #
-        #     except Exception as e:
-        #         self.send_response(500)
-        #         self.end_headers()
-        #         self.wfile.write(f"Внутренняя ошибка сервера: {e}".encode("utf-8"))
-        # else:
-        #     self.send_response(404)
-        #     self.end_headers()
-        #     self.wfile.write(b"Not Found")
-
-
 if __name__ == "__main__":
 
     # Инициализация веб-сервера, который будет по заданным параметрах в сети
